6b1857eebff40d4d48443297c95d49b4d20043e3
qyt <486179@qq.com>
2024-08-01 14:49:45 +0800

    added:tinyxml2 module

---
+++ XEngine_Example/TinyXML2_APPExample/TinyXML2_APPExample.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include "../../XEngine_Module/tinyxml2/tinyxml2.h"
+
+#ifdef _DEBUG
+#pragma comment(lib,"../../Debug/tinyxml2")
+#else
+#pragma comment(lib,"../../Release/tinyxml2")
+#endif
+
+int main()
+{
+    tinyxml2::XMLDocument m_XMLDocument;
+    m_XMLDocument.LoadFile("1.xml");
+
+
+    return 0;
+}
